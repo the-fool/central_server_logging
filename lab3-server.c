1117a30ba5afe3401966e3577f26f591c5774db1
Thomas Ruble <sketchbang@gmail.com>
2015-11-05 15:42:27 +0000

    call pipe

--- lab3-server.c
+++ lab3-server.c
@@ -4,18 +4,26 @@
 #include <errno.h>
 
 #define NUMBER_CLIENTS 4
+#define MSG_BUF 120
 
 char *readline(int fd);
 
 int main(int argc, char *argv[])
 {
+  int pfd[2], i, ret;
+  char msg[MSG_BUF];
+  pid_t pid;
+
   if (argc != 2) {
     fprintf(stderr, "Usage: lab3 <filenam>\n");
     return EXIT_FAILURE;
   }
   
-  int i;
-  pid_t pid;
+  if (pipe(pfd) != 0) {
+    perror("Error creating pipe");
+    return EXIT_FAILURE;
+  }
+  
   for (i = 0; i < NUMBER_CLIENTS; i++) {
     switch (pid = fork()) 
       {
